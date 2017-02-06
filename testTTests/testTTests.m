17aa10ec48268256d8db66d1eeec7c1026a47357
Chris Mitchelmore <chrismitchelmore@gmail.com>
2017-02-06 20:13:30 +0000

    Initial Commit

---
+++ testTTests/testTTests.m
@@ -0,0 +1,39 @@
+//
+//  testTTests.m
+//  testTTests
+//
+//  Created by Chris Mitchelmore on 06/02/2017.
+//  Copyright © 2017 Chris Mitchelmore. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testTTests : XCTestCase
+
+@end
+
+@implementation testTTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
